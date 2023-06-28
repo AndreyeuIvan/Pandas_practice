a9f75c15068070b37c9b1b95c696d6634594cf07
Ivan Andreyeu <58092467+AndreyeuIvan@users.noreply.github.com>
2023-06-28 23:18:01 +0300

    Update my_practice.py
    Added options related to na, dropna, fillna, query.

--- my_practice.py
+++ my_practice.py
@@ -1,5 +1,15 @@
-df = pd.DataFrame({x:x^2 for x in range(1,6)})
+import pandas as pd
 
+
+series = pd.Series(
+    [x for x in range(1,6)],
+     index = ['row_1', 'row_2', 'row_3', 'row_4', 'row_5'],
+     name = 'my_list_comp'
+)
+
+#creating DataFrames
+
+df = pd.DataFrame({x:x^2 for x in range(1,6)})
 
 #indexed
 
@@ -39,3 +49,39 @@ df.set_index(df['data'], inplace=True)
 
 series = df['name']
 df_1 = df[['name']]
+
+df[ "new_col" ] = df["age"] # change columns value
+
+# Multiindexes
+
+arr = [['a', 's', 'd', 'f'], [1, 2, 3, 4]]
+int = pd.MultiIndex.from_arrays(arr)
+
+# filtering df ! It is better to use query() also @ is used for assigning element
+
+new_df = df[(df['стаж работы'] > 4) & (df['зарплата'] < 95000)] 
+
+new_df = df.query('`стаж работы` <= 4')
+
+new_df = df.query('not `год рождения` > 91 or `зарплата` > 70000')
+
+# working with na
+
+new_df = df.dropna(how='any') # any for 1 or more Na in a row(axis=0) or column(axis=1)
+new_df = df.dropna(subset=['name', 'age']) # sub set stands for coluns, what we are working with/ axis=1 stand for row, subset[1]
+new_df = df.dropna(subset=[2, 3, 7], axis=1) # on indexes
+
+df.fillna(value=
+    {
+    'name': 'No_name',
+    'count': 10
+    },
+    inplace = True
+)
+# for providing columns and rows we are using k:v pair, as column(k) and row(v)
+
+# isna()
+
+new_df = df[df['name'].isna()]
+
+new_df = df[~ df['name'].isna() & (df['count'] <= 5)]
